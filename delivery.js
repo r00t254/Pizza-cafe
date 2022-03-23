66bafc1e650620981c2369e5ecf4b45b020f5bec
r00t254 <root254.mary@gmail.com>
2022-03-23 13:43:42 +0300

    add changes

--- delivery.js
+++ delivery.js
@@ -1,16 +1,22 @@
-{$(Document).ready(function);
+function constructor(size,toppins,crust,order,quantity){
+  this.size = size;
+  this.toppins = toppins;
+  this.crust = crust;
+  this.order = ordernumber;
+  this.quantity = quantity;
+  }
+$(document).ready(function(){
+  $("#submit").click(function(event){
+    event.preventDefault();
+
+
 var size =$("#size option:select").val();
 var crust =$("#crust option:select").val();
 var toppins =$("#toppins option:select").val();
 var ordernumber =$("ordernumber option:select").val();
 var quantity =parseInt(size)*parseInt(crust)*parseInt(toppins)
 
-constructor(size,toppins,crust,order,quantity)
-this.size = size;
-this.toppins = toppins;
-this.crust = crust;
-this.order = ordernumber;
-this.quantity = quantity;}
+
 $("#size").html($("#size option:selected").text() + " - " + size);
   $("#toppins").html($("#toppins option:selected").text() + " - " + toppins);
   $("#crust").html($("#crust option:selected").text() + " - " + crust);
@@ -21,8 +27,8 @@ $("#size").html($("#size option:selected").text() + " - " + size);
     var toppins = $("#toppins option:selected").val();
     var crust = $(".crust option:selected").val();
     var quantity = parseInt(size) + parseInt(toppins) + parseInt(crust);
-    // order = order + 1;
-    // amountt = amountt + total;
+    order = 1;
+    amountt = 0;
     var newPizza = new constructor(size, toppins, crust, total, quantity);
     var newRow = '<tr><th scope="row">' + newPizza.ordernumber + '</th><td id="pizzaSize">' + $(".pizzaSize option:selected").text() + " - " + newPizza.size + '</td><td id="pizzaTopping">' + $(".pizzaTopping option:selected").text() + " - " + newPizza.toppings + '</td><td id="pizzaCrust">' + $(".pizzaCrust option:selected").text() + " - " + newPizza.crust + '</td><td id="quantity">' + newPizza.quantity + '</td></tr>'
     $("#pizza").append(newRow);
@@ -35,9 +41,8 @@ $("#size").html($("#size option:selected").text() + " - " + size);
   //   } else if (quantity === 1) {
     
   //     alert("Thank you for choosing pizza Inn your cost is ksh " + total3.initialCost());
-    }
-});
-
+})
+})
 
 
 
